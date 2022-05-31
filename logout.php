52ba553fa46d81d35b8dc727c3da89bb469c097d
Jamie Whittingham <jamie.whittingham@gmail.com>
2022-05-31 12:03:58 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,30 +1,8 @@
 <?php
 
-// error logging
-ini_set( 'display_startup_errors', 1 );
-ini_set( 'display_errors', 1 );
-ini_set( 'error_reporting', E_ALL );
-error_reporting( E_ALL );
-
-// session start
-session_start();
-
-// start timer for page loaded var
-$time = microtime();
-$time = explode( ' ', $time );
-$time = $time[1] + $time[0];
-$start = $time;
-
-// vars
-ini_set("default_socket_timeout", 15);
-ini_set("memory_limit", -1);
-$globals['dev']             = true;
-$globals['basedir']         = '/home/cloudshield/public_html/dashboard/';
-
 // include main functions
-include( $globals['basedir'].'includes/db.php' );
-include( $globals['basedir'].'includes/globals.php' );
-include( $globals['basedir'].'includes/functions.php' );
+include( dirname(__FILE__).'/includes/core.php' );
+include( dirname(__FILE__).'/includes/functions.php' );
 
 // session destroy
 session_destroy();
