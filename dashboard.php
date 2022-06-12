509900cf95819f00755158fbee7b6bf7e3460cfd
Jamie Whittingham <jamie.whittingham@gmail.com>
2022-06-12 08:47:45 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1760,9 +1760,7 @@ if( isset( $platform_text[1] ) ) {
 								<h4 class="panel-title">Projects</h4>
 								<div class="panel-heading-btn">
 									<div class="btn-group">
-										<?php if( $account_details['can_create_projects'] == 'yes') { ?>
-			        						<button class="btn btn-xs btn-green" data-toggle="modal" data-target="#project_add_modal">Add a Project</button>
-										<?php } ?>
+			        					<button class="btn btn-xs btn-green" data-toggle="modal" data-target="#project_add_modal">Add a Project</button>
 										<button class="btn btn-xs btn-green" data-toggle="modal" data-target="#project_join_modal">Join a Project</button>
 										<a href="javascript:void(0);" class="btn btn-xs btn-info" onclick="tutorial();">Tutorial &amp; Help</a>
 									</div>
@@ -1771,11 +1769,7 @@ if( isset( $platform_text[1] ) ) {
 							<div class="panel-body">
 								<?php if( !isset( $projects[0]['id'] ) ) { ?>
 									<center>
-										<?php if( $account_details['can_create_projects'] == 'yes') { ?>
-											<h2>You need to add a Project.</h2>
-										<?php } else { ?>
-											<h2>You need to join a Project.</h2>
-										<?php } ?>
+									<h2>You need to add or join a Project.</h2>
 									</center>
 								<?php } else { ?>
 									<table id="table_clusters" class="table table-striped table-bordered table-td-valign-middle">
