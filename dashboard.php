ae1ca7579c74d3cd4ee1ed739b6d8d0f5a35b3a9
Jamie Whittingham <jamie.whittingham@gmail.com>
2022-06-12 02:47:40 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -3114,7 +3114,7 @@ if( !isset( $_SESSION['logged_in'] ) || $_SESSION['logged_in'] != true ) {
 		</script>
 	<?php } ?>
 
-	<?php if( $account_details['local_data']['accept_terms'] == 'no' ){ ?>
+	<?php if( $account_details['accept_terms'] == 'no' ){ ?>
 		<script>
 		    $( window).on( 'load',function() {
                 $( '#modal-terms' ).modal( { 
